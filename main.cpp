c88a280bcac1289e2f075ef9115b7e3cd326f07f
Tion <tion.jamesdominic@gmail.com>
2025-11-05 10:22:16 +0800

    Ideas first

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+void regexToNFA(){
+
+    
+    
+    
+
+}
+
+int main(){
+    string DNA = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"; // test sample
+    string RegularExpression;
+
+    cout << "Enter a RegEx: ";
+    cin >> RegularExpression;
+
+
+
+    return 0;
+}
